0a7fcbfc8503561a69708872dcfc77d84a171185
softo-sohit <sohit.rana@softomation.com>
2025-06-20 16:15:01 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,22 @@
+# INTEGER
+age=35
+print(type(age))
+
+# floating type
+
+height=5.1
+print(type(height))
+
+# string
+name ="paras"
+print(type(name))
+
+# boolean 
+is_true=True
+print(type(is_true))
+
+a=10
+b=10
+a==b
+print(type(a==b))
+
---
+++ l.py
@@ -0,0 +1,22 @@
+# for loop 
+#range(5) (function)
+for i in range(5):
+    print(i)
+
+# Different range
+
+for i in range(1,6):
+    print(i)
+
+#step one to one
+#range(1,20,1)
+for i in  range(1,20,1):
+    print(i)
+
+#step one to one
+#range(1,20,2)
+for i in  range(1,20,2):
+    print(i)
+
+for i in  range(1,20,3):
+    print(i)
